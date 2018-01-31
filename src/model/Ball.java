199d38d0b0cdd883e028a3e798f6e84e0b2fc43d
U-DS\cwb15142 <cwb15142@CIS-ARROL-35.cis.strath.ac.uk>
2018-01-31 15:54:50 +0000

    adding testing model

--- src/model/Ball.java
+++ src/model/Ball.java
@@ -1,4 +1,14 @@
 package model;
 
 public class Ball {
+
+    private double radius;
+
+    public Ball(double radius){
+        this.radius = radius;
+    }
+
+    public double getRadius(){
+        return radius;
+    }
 }
