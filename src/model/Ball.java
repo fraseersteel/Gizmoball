dfe86f0d2aaded7f5fdcb9ee2e330a8fe7600cde
Graeme Kerr <fsb15185@uni.strath.ac.uk>
2018-02-07 14:19:05 +0000

    getters and setters and vectors, oh my

--- src/model/Ball.java
+++ src/model/Ball.java
@@ -7,18 +7,68 @@ public class Ball {
 
     private String id;
     private Vect velocity;
-    private double xPosition, yPosition;
+    private double xPosition;
+    private double yPosition;
     private double radius;
 
-    public Ball(String id, double radius, double xPosition, double yPosition){
+    public Ball(String id, double radius, double xPosition, double yPosition, double xVelocity, double yVelocity){
 
         this.id = id;
         this.radius = radius;
         this.xPosition = xPosition;
         this.yPosition = yPosition;
+        velocity = new Vect(xVelocity, yVelocity);
+    }
+
+    // constructor for balls of fixed size? would ideally want a maximum size for user specified balls
+    // to avoid, uh, shenanigans
+    // would have to test how big they can get before it just sort of breaks
+
+    public String getId(){
+        return id;
+    }
+
+    public void setId(String i){
+        id = i;
     }
 
     public double getRadius(){
         return radius;
+    }
+
+    public void setRadius(double r){
+        radius = r;
+    }
+
+    public double getXPos(){
+        return xPosition;
+    }
+
+    public void setXPos(double x){
+        xPosition = x;
+    }
+
+    public double getYPos(){
+        return yPosition;
+    }
+
+    public void setYPos(double y){
+        yPosition = y;
+    }
+
+    public Vect getVelocity(){
+        return velocity;
+
+    }
+    public double getXVelo(){
+        return velocity.x();
+    }
+
+    public double getYVelo(){
+        return velocity.y();
+    }
+
+    public void setVelocity(Vect v){
+        velocity = v;
     }
 }
