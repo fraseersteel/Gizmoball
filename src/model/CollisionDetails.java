d2a82f86bcd4492b545f8f294e6e029c6df6cb4c
Graeme Kerr <fsb15185@uni.strath.ac.uk>
2018-02-07 15:34:26 +0000

    Added the moveBall method from the lecture slides

--- src/model/CollisionDetails.java
+++ src/model/CollisionDetails.java
@@ -2,4 +2,5 @@ package model;
 
 public class CollisionDetails {
     //per the moveBall method from the slides
+    //precise details - the time until collision obviously and the velocity at which the collision occurs?
 }
